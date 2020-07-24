3602e9fd05a559787a4066c881763106f79f6e04
Areukiddin <evgeny.maxi@gmail.com>
2020-07-24 17:41:59 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,34 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Main } from "./pages/Main/Main";
-import { DialogsContainer } from "./pages/Dialogs/DialogsContainer";
 import { About } from "./pages/About/About";
 import { Navbar } from "./navigation/Navbar";
-
 import { Gallery } from "./pages/Gallery/Gallery";
+import DialogsContainer from "./pages/Dialogs/DialogsContainer";
+import UsersContainer from "./pages/Users/UsersContainer";
 
 function App(props) {
-  let state = props.store.getState();
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container pt-4">
         <Switch>
-          <Route
-            path={"/"}
-            exact
-            render={() => (
-              <Main
-                userInfo={state.userPage.userInfo}
-                dispatch={props.dispatch}
-                myFeed={state.userPage.myFeed}
-              />
-            )}
-          />
+          <Route path={"/"} exact render={() => <Main store={props.store} />} />
           <Route path={"/gallery"} render={() => <Gallery />} />
           <Route
             path={"/dialogs"}
             render={() => <DialogsContainer store={props.store} />}
           />
           <Route path={"/about"} render={() => <About />} />
+          <Route
+            path={"/users"}
+            render={() => <UsersContainer store={props.store} />}
+          />
         </Switch>
       </div>
     </BrowserRouter>
   );
 }
 
+/* Комментарий */
 export default App;
